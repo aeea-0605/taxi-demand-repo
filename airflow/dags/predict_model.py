d7d7fee2de3220af0344c11d88c7c5b95ebf9f06
aeea-0605 <tmdwnrj96@gmail.com>
2021-10-19 20:43:42 +0900

    predict demand DAG

--- airflow/dags/predict_model.py
+++ airflow/dags/predict_model.py
@@ -1,34 +1,35 @@
 from datetime import datetime, timedelta
-from airflow import models
-from airflow.operators.bash_operator import BashOperator
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
 
 
 default_args = {
     'owner': 'seungju',
     'depends_on_past': False,
-    'start_date': datetime(2021, 10, 5),
+    'start_date': days_ago(1),
     'email': ['tmdwnrj96@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 0,
     'retry_delay': timedelta(minutes=5),
-    'project_id': 'insert your google cloud project-id'
+    'project_id': 'bigquery-project-327214'
 }
 
-bash_command = """
-    python3 /home/airflow/gcs/data/taxi-demand-prediction/main.py --mode predict --dev_env production
-"""
-
-with models.DAG(
+with DAG(
         dag_id='Predict-taxi_demand_every_week',
         description='Predict taxi demand',
-        schedule_interval='0 * * * *',
+        schedule_interval='0 */3 * * *',
         default_args=default_args) as dag:
-
+    bash_command = "python3 /home/airflow/gcs/data/taxi-demand-prediction/main.py --mode predict --dev_env production"
     predict_operator = BashOperator(
-        dag=dag,
         task_id='predict_demand',
         bash_command=bash_command
     )
+    
+    complete_task = BashOperator(
+        task_id='predict_demand_complete',
+        bash_command='echo "Prediction Done!!"'
+    )
 
-    predict_operator
+    predict_operator >> complete_task
