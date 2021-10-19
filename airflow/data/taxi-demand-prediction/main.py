c103ac4cca2dde4bc44dd32c912800dc2e3996a6
aeea-0605 <tmdwnrj96@gmail.com>
2021-10-19 20:53:29 +0900

    main file

--- airflow/data/taxi-demand-prediction/main.py
+++ airflow/data/taxi-demand-prediction/main.py
@@ -1,12 +1,13 @@
 import argparse
 import os
+import numpy as np
 import pandas as pd
 import sklearn
 from sklearn.preprocessing import LabelEncoder
 
-import rf_trainer
+import reg_trainer
 from base_data_query import base_query
-from utils import init_config, split_train_and_test
+from modules import init_config, split_train_and_test_period, split_x_and_y_log
 
 
 parser = argparse.ArgumentParser()
@@ -16,7 +17,7 @@ parser.add_argument("--mode", help="[train], [predict]", type=str, default="pred
 flag = parser.parse_args()
 
 config = init_config(flag.dev_env)
-print(config)
+
 model_dir = f"{config['save_folder']}/models/"
 
 # Feature Engineering (Using BigQuery)
@@ -24,25 +25,22 @@ print('load data')
 base_df = pd.read_gbq(query=base_query, dialect='standard', project_id=config['project'])
 
 # Data Preprocessing (Label Encoding)
-zip_code_le = LabelEncoder()
-base_df['zip_code_le'] = zip_code_le.fit_transform(base_df['zip_code'])
+le = LabelEncoder()
+base_df['zip_code_le'] = le.fit_transform(base_df['zip_code'])
+
+# log scaling >> 0인 값이 존재하기에 log1p를 사용해 inf값이 존재하지 않게함
+base_df['log_cnt'] = np.log1p(base_df['cnt'])
 
 # Split Train and Test
-train_df, test_df = split_train_and_test(base_df, '2015-01-24')
+train_df, test_df = split_train_and_test_period(base_df, 7)
 print('data split Done')
 
-# Delete Columns, Values & Split X, y
-train_df.drop(columns=['zip_code', 'pickup_hour'], inplace=True)
-test_df.drop(columns=['zip_code', 'pickup_hour'], inplace=True)
-
-y_train_raw = train_df.pop('cnt')
-y_test_raw = test_df.pop('cnt')
-
+# Null값에 대해 각 행 기준 다음 행의 값을 채우고, 다음 행이 없다면 0값을 채움
 train_df = train_df.fillna(method='backfill')
 test_df = test_df.fillna(method='backfill')
 
-x_train = train_df.copy()
-x_test = test_df.copy()
+# Split X, y
+x_train, x_test, y_train_log, _, y_test_log = split_x_and_y_log(train_df, test_df, y='log_cnt')
 
 # Run Modeling
 if __name__ == '__main__':
@@ -50,11 +48,11 @@ if __name__ == '__main__':
         os.mkdir(model_dir)
     if flag.mode == 'train':
         print('train start')
-        train_op = rf_trainer.Trainer(config)
-        train_op.train(x_train, y_train_raw)
+        train_op = reg_trainer.Trainer(config)
+        train_op.train(x_train, y_train_log)
     elif flag.mode == 'predict':
         print('predict start')
-        train_op = rf_trainer.Trainer(config)
-        train_op.predict(x_test, y_test)
+        train_op = reg_trainer.Trainer(config)
+        train_op.predict(x_test, y_test_log)
     else:
-        raise KeyError(f"Incorrect value flog.mode = {flog.mode}")
+        raise KeyError(f"Incorrect value flag.mode = {flag.mode}")
